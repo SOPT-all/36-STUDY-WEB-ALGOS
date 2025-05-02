13ef53fac5dc5d82ed715988e0aaaa430846b607
이윤지 <rhaxlddbswl3@gmail.com>
2025-05-03 03:39:53 +0900

    refactor: 메모리초과 해결

--- YoonjiLee/week2/2751.js
+++ YoonjiLee/week2/2751.js
@@ -1,3 +1,32 @@
+// const fs = require("fs");
+// const input = fs
+//   .readFileSync("run.txt")
+//   .toString()
+//   .trim()
+//   .split("\n")
+//   .map(Number);
+// const n = input.shift();
+
+// function quickSort(arr) {
+//   if (arr.length <= 1) return arr;
+
+//   const pivot = arr[Math.floor(arr.length / 2)];
+//   const left = [],
+//     right = [],
+//     equal = [];
+
+//   for (let num of arr) {
+//     if (num < pivot) left.push(num);
+//     else if (num > pivot) right.push(num);
+//     else equal.push(num);
+//   }
+
+//   return [...quickSort(left), ...equal, ...quickSort(right)];
+// }
+
+// const sorted = quickSort(input);
+// console.log(sorted.join("\n"));
+
 const fs = require("fs");
 const input = fs
   .readFileSync("run.txt")
@@ -5,24 +34,7 @@ const input = fs
   .trim()
   .split("\n")
   .map(Number);
-const n = input.shift();
+input.shift(); 
 
-function quickSort(arr) {
-  if (arr.length <= 1) return arr;
-
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const left = [],
-    right = [],
-    equal = [];
-
-  for (let num of arr) {
-    if (num < pivot) left.push(num);
-    else if (num > pivot) right.push(num);
-    else equal.push(num);
-  }
-
-  return [...quickSort(left), ...equal, ...quickSort(right)];
-}
-
-const sorted = quickSort(input);
-console.log(sorted.join("\n"));
+input.sort((a, b) => a - b);
+console.log(input.join("\n"));
