19555f75ca4a3cac5d4480984e38b762c6bc0c4e
봄 <saebom@Saebomui-MacBookPro.local>
2025-05-03 15:30:17 +0900

    feat: 1427 문제 풀이

--- Kwonsaebom/Week1/problem2.js
+++ Kwonsaebom/Week1/problem2.js
@@ -1,1 +1,31 @@
 // https://www.acmicpc.net/problem/1427
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let arr = [];
+
+rl.on("line", (line) => {
+  arr = line.split("").map(Number);
+  rl.close();
+});
+
+rl.on("close", () => {
+  let temp = 0;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log(arr.join(""));
+});
