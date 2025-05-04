04056cb368d2a4a19e66b4e239fcbba8511bd815
승우 <hsu235@naver.com>
2025-05-04 22:14:36 +0900

    feat : 1427 문제풀이
    feat : 1427 문제풀이

--- Hanseungwoo/Week1/problem2.py
+++ Hanseungwoo/Week1/problem2.py
@@ -1,1 +1,17 @@
 # https://www.acmicpc.net/problem/1427
+
+n = input()      
+nums = []         
+
+for num in n:
+    nums.append(int(num))
+
+for i in range(len(nums)):
+    max_idx = i
+    for j in range(i + 1, len(nums)):
+        if nums[j] > nums[max_idx]:    
+            max_idx = j
+    nums[i], nums[max_idx] = nums[max_idx], nums[i]  
+
+for fin in nums:
+    print(fin, end='')
