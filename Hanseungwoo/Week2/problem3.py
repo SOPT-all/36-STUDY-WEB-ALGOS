d06f1c730fb3a1d9282b2e40813804f5f517cdd8
승우 <hsu235@naver.com>
2025-05-05 23:59:38 +0900

    feat : 2주차 문제풀이 방식 설명
    feat : 2주차 문제풀이 방식 설명

--- Hanseungwoo/Week2/problem3.py
+++ Hanseungwoo/Week2/problem3.py
@@ -2,18 +2,20 @@
 
 import heapq
 
-n = int(input()) 
+n = int(input()) # 명령의 개수 입력받기
 
-h = []
+h = []  # 최대 힙으로 사용할 리스트
 
 for _ in range(n):
-    x = int(input())  
+    x = int(input())   # 명령 하나 입력받기
 
     if x == 0:
+        # 0이면 현재 힙에서 가장 큰 값을 꺼내 출력해야 함
         if len(h) == 0:
-            print(0)
+            print(0)  # 힙이 비어있으면 0 출력
         else:
             num = heapq.heappop(h)
-            print(-num)
+            print(-num)  # 음수로 저장했으므로 다시 양수로 바꿔서 출력
     else:
+        # x가 0이 아닌 경우에 힙에 추가
         heapq.heappush(h, -x)
