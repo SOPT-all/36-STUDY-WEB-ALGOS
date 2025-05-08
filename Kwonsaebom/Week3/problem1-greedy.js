cf247d6e0dfa8ce0e686d46731d0d2aab1f5e1b9
Saebom <spring180@naver.com>
2025-05-08 20:24:08 +0900

    feat: 1931  문제 풀이

---
+++ Kwonsaebom/Week3/problem1-greedy.js
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/1931
+
+const fs = require("fs");
+const input = fs
+  .readFileSync("input.txt", "utf-8")
+  .toString()
+  .trim()
+  .split("\n");
+
+const times = input.slice(1).map((line) => line.split(" ").map(Number));
+
+const sortedTimes = times.sort((a, b) => {
+  if (a[1] === b[1]) return a[0] - b[0];
+  return a[1] - b[1];
+});
+
+let count = 0;
+let endTime = 0;
+
+for (let i = 0; i < sortedTimes.length; i++) {
+  const [start, end] = sortedTimes[i];
+  if (endTime <= start) {
+    endTime = end;
+    count++;
+  }
+}
+
+console.log(count);
