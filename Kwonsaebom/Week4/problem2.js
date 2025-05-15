83aa66f6baa6bcc91f06996296521d1c68984c93
Saebom <spring180@naver.com>
2025-05-16 00:56:13 +0900

    feat: 1764 문제 풀이

--- Kwonsaebom/Week4/problem2.js
+++ Kwonsaebom/Week4/problem2.js
@@ -1,1 +1,25 @@
 // https://www.acmicpc.net/problem/1764
+
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [dmNumber, bmNumber] = input[0].split(" ").map(Number);
+
+const dmPerson = input.slice(1, 1 + dmNumber);
+const bmPerson = input.slice(1 + dmNumber);
+
+const dmSet = new Set(dmPerson);
+const result = [];
+
+for (let person of bmPerson) {
+  if (dmSet.has(person)) {
+    result.push(person);
+  }
+}
+
+result.sort();
+
+console.log(result.length);
+for (r of result) {
+  console.log(r);
+}
